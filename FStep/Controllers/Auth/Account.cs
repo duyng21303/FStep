a0364e77a0631adf736b056889f8463a01a7a696
Nguyễn Duy <133040402+duyng21303@users.noreply.github.com>
2024-05-22 00:39:20 +0700

    #8 - login, logout (#13)

---
+++ FStep/Controllers/Auth/Account.cs
@@ -0,0 +1,82 @@
+﻿using FStep.Data;
+using FStep.ViewModels;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FStep.Controllers.Auth
+{
+	public class Account : Controller
+	{
+		private readonly Fstep1Context db;
+
+		public Account(Fstep1Context context) 
+		{
+			db = context;
+		}
+		[HttpGet]
+		public IActionResult Login(string? ReturnUrl)
+		{
+			ViewBag.ReturnUrl = ReturnUrl;
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginVM model, string? ReturnUrl)
+		{
+			ViewBag.ReturnUrl = ReturnUrl;
+			if(ModelState.IsValid)
+			{
+				var user = db.Users.SingleOrDefault(user => user.IdUser == model.userName);
+				if(user == null)
+				{
+					ModelState.AddModelError("Error", "Sai tên đăng nhập hoặc mật khẩu");
+				}
+				else
+				{
+					if (user.Status == false)
+					{
+						ModelState.AddModelError("Error", "Tài khoản đã bị khóa, vui lòng liên hệ Admin hoặc Moder");
+					}
+					else
+					{
+						if(user.Password == model.password)
+						{
+							ModelState.AddModelError("Error", "Sai tên đăng nhập hoặc mật khẩu");
+						}
+						else
+						{
+							var claims = new List<Claim>
+							{
+								new Claim(ClaimTypes.Email, user.Email),
+								new Claim(ClaimTypes.Name, user.Name),
+								new Claim("UserID", user.IdUser),
+                                //claim-role động
+                                new Claim(ClaimTypes.Role, user.IdRoleNavigation.RoleName)
+							};
+							var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+							var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+							await HttpContext.SignInAsync(claimsPrincipal);
+							if (Url.IsLocalUrl(ReturnUrl))
+							{
+								return Redirect(ReturnUrl);
+							}
+							else
+							{
+								return Redirect("/");
+							}
+						}
+					}
+				}
+			}
+			return View();
+		}
+		[Authorize]
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync();
+			return Redirect("/");
+		}
+	}
+}
