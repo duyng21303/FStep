ef73a9b97ac085b61c252a231dda33e511f9bfe8
Nguyễn Duy <133040402+duyng21303@users.noreply.github.com>
2024-05-22 02:15:18 +0700

    update datatype role_name (#9) (#14)

--- FStep/Data/Role.cs
+++ FStep/Data/Role.cs
@@ -7,7 +7,7 @@ public partial class Role
 {
     public int IdRole { get; set; }
 
-    public int? RoleName { get; set; }
+    public string? RoleName { get; set; }
 
     public virtual ICollection<User> Users { get; set; } = new List<User>();
 }
