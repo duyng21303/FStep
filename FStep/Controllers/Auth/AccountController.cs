e9c18a2dd3fa9119a9af5d3aedd36e6045d3760b
duyng21303 <duyng21303@gmail.com>
2024-05-28 14:26:54 +0700

    .

--- FStep/Controllers/Auth/AccountController.cs
+++ FStep/Controllers/Auth/AccountController.cs
@@ -8,24 +8,26 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using FStep.Helpers;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace FStep.Controllers.Auth
 {
 	public class AccountController : Controller
 	{
 		private readonly Fstep1Context db;
-		private readonly SignInManager<IdentityUser> _signInManager;
-		private readonly UserManager<IdentityUser> _userManager;
 
 		public AccountController(Fstep1Context context)
 		{
 			db = context;
+
 		}
 		[HttpGet]
 		[AllowAnonymous]
 		public IActionResult Login(string? ReturnUrl)
 		{
 			ViewBag.ReturnUrl = ReturnUrl;
+
 			return View();
 		}
 		[HttpPost]
@@ -89,55 +91,6 @@ namespace FStep.Controllers.Auth
 		{
 			return View();
 		}
-		[HttpPost]
-		[AllowAnonymous]
-		public IActionResult ExternalLogin(string provider, string returnUrl = null)
-		{
-			var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { returnUrl });
-			var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-			return Challenge(properties, provider);
-		}
 
-		[HttpGet]
-		[AllowAnonymous]
-		public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
-		{
-			if (remoteError != null)
-			{
-				ModelState.AddModelError(string.Empty, $"Error from external provider: {remoteError}");
-				return RedirectToAction(nameof(Login));
-			}
-
-			var info = await _signInManager.GetExternalLoginInfoAsync();
-			if (info == null)
-			{
-				return RedirectToAction(nameof(Login));
-			}
-
-			var signInResult = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false);
-			if (signInResult.Succeeded)
-			{
-				return LocalRedirect(returnUrl ?? "/");
-			}
-			else
-			{
-				var email = info.Principal.FindFirstValue(ClaimTypes.Email);
-				var name = info.Principal.FindFirstValue(ClaimTypes.Name);
-				var id = info.Principal.FindFirstValue(ClaimTypes.Email);
-
-				var user = new IdentityUser { UserName = email, Email = email, NormalizedUserName = name };
-				var result = await _userManager.CreateAsync(user);
-				if (result.Succeeded)
-				{
-					result = await _userManager.AddLoginAsync(user, info);
-					if (result.Succeeded)
-					{
-						await _signInManager.SignInAsync(user, isPersistent: false);
-						return LocalRedirect(returnUrl ?? "/");
-					}
-				}
-				return RedirectToAction(nameof(Login));
-			}
-		}
 	}
 }
