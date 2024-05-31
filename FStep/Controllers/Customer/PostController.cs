c5b270ec50eb147373a8f4be0da1c63469b483ed
duyng21303 <duyng21303@gmail.com>
2024-06-01 02:15:13 +0700

    merge

--- FStep/Controllers/Customer/PostController.cs
+++ FStep/Controllers/Customer/PostController.cs
@@ -10,64 +10,64 @@ using System.Security.Claims;
 
 namespace FStep.Controllers.Customer
 {
-	public class PostController : Controller
-	{
-		private readonly FstepContext db;
-		private readonly IMapper _mapper;
+    public class PostController : Controller
+    {
+        private readonly FstepDBContext db;
+        private readonly IMapper _mapper;
 
-		public PostController(FstepContext context, IMapper mapper)
-		{
-			db = context;
-			_mapper = mapper;
-		}
+        public PostController(FstepDBContext context, IMapper mapper)
+        {
+            db = context;
+            _mapper = mapper;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		[HttpGet]
-		public IActionResult CreatePost()
-		{
-			return View();
-		}
-		//Create post
-		[Authorize]
-		[HttpPost]
-		public IActionResult CreatePost(PostVM model, IFormFile img)
-		{
-			try
-			{
-				var product = _mapper.Map<Product>(model);
-				product.Name = model.NameProduct;
-				product.Quantity = model.Quantity;
-				product.Price = model.Price;
-				product.Status = true;
-				product.Detail = model.DetailProduct;
-				db.Add(product);
-				db.SaveChanges();
+        [HttpGet]
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+        //Create post
+        [Authorize]
+        [HttpPost]
+        public IActionResult CreatePost(PostVM model, IFormFile img)
+        {
+            try
+            {
+                var product = _mapper.Map<Product>(model);
+                product.Name = model.NameProduct;
+                product.Quantity = model.Quantity;
+                product.Price = model.Price;
+                product.Status = true;
+                product.Detail = model.DetailProduct;
+                db.Add(product);
+                db.SaveChanges();
 
-				var post = _mapper.Map<Post>(model);
-				post.Content = model.Content;
-				post.Date = DateTime.Now;
-				//Helpers.Util.UpLoadImg(model.Img, "")
-				post.Img = Util.UpLoadImg(img, "postPic");
-				post.Status = true;
-				post.Type = model.Type;
-				post.Detail = model.Detail;
-				post.IdUser = User.FindFirst("UserID").Value;
-				//get IdProduct from database map to Post
-				post.IdProduct = db.Products.Max(p => p.IdProduct);
-				db.Add(post);
-				db.SaveChanges();
-				return Redirect("/");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex);
-			}
+                var post = _mapper.Map<Post>(model);
+                post.Content = model.Content;
+                post.Date = DateTime.Now;
+                //Helpers.Util.UpLoadImg(model.Img, "")
+                post.Img = Util.UpLoadImg(img, "postPic");
+                post.Status = true;
+                post.Type = model.Type;
+                post.Detail = model.Detail;
+                post.IdUser = User.FindFirst("UserID").Value;
+                //get IdProduct from database map to Post
+                post.IdProduct = db.Products.Max(p => p.IdProduct);
+                db.Add(post);
+                db.SaveChanges();
+                return Redirect("/");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
