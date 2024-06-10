1fe93c08afe3e94531dc991082e02edf59479667
duyng21303 <duyng21303@gmail.com>
2024-06-10 11:36:47 +0700

    resole conflict

--- FStep/wwwroot/js/site.js
+++ FStep/wwwroot/js/site.js
@@ -3,43 +3,7 @@
 
 // Write your JavaScript code.
 // Lấy tất cả các nút cộng và trừ
-const minusBtn = document.querySelector(".qty-btn.minus");
-const plusBtn = document.querySelector(".qty-btn.plus");
 
-// Lấy input
-const inputQty = document.querySelector(".pro-qty input");
-
-// Thêm sự kiện khi click vào nút trừ
-minusBtn.addEventListener("click", function () {
-    let value = parseInt(inputQty.value);
-    if (value > 1) {
-        inputQty.value = value - 1;
-    }
-});
-
-// Thêm sự kiện khi click vào nút cộng
-plusBtn.addEventListener("click", function () {
-    let value = parseInt(inputQty.value);
-    inputQty.value = value + 1;
-});
-//comment
-document.querySelector('.button__cmt-send').addEventListener('click', function () {
-    var commentText = document.querySelector('.textarea').value;
-
-    fetch('/your-controller/SubmitComment', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ commentText: commentText }),
-    })
-        .then(response => {
-           
-        })
-        .catch(error => {
-            
-        });
-});
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
