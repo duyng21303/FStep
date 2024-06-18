7ddafccfcc4bcba1b30940e4c9bcfcd9349f2f16
duyng21303 <duyng21303@gmail.com>
2024-06-19 00:41:05 +0700

    fix index

--- FStep/wwwroot/js/site.js
+++ FStep/wwwroot/js/site.js
@@ -231,7 +231,7 @@ connection.on("LoadMessages", function (messages, currentUser, recieverUser, con
                     <!-- Hình ảnh sản phẩm nhỏ hơn -->
                     <img src="/img/postPic/${confirm.post.img}" alt="Product Image" class="img-thumbnail" style="width: 50px; height: 50px; min-height: 50px; min-width: 50px">
                 </div>
-                <div class="col text-left p-2">
+                <div class="col text-left p-2 ml-12">
 
                     <!-- Tên sản phẩm và chi tiết, giảm kích thước chữ -->
                     <h6 class="product-name m-0" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap; max-width: 200px;"">${confirm.post.content}</h6>
