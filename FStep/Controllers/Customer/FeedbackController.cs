c815f3a165ef82b806eda4b7e4e774ad6760dfae
dhung2103 <hunglupu@gmail.com>
2024-06-30 02:43:46 +0700

    update report for comment and transaction

--- FStep/Controllers/Customer/FeedbackController.cs
+++ FStep/Controllers/Customer/FeedbackController.cs
@@ -43,7 +43,7 @@ namespace FStep.Controllers.Customer
         {
             try
             {
-                // TODO: Sửa lại nếu người dùng đã đánh giá rồi thì không cho đánh giá nữa
+                // TODO: Sửa lại nếu người dùng đã đánh giá rồi thì ko cho đánh giá nữa
                 // Bổ sung feeback theo trans
                 FeedbackVM info = HttpContext.Session.Get<FeedbackVM>("FEEDBACK_INFO");
                 var feedback = new Feedback();
