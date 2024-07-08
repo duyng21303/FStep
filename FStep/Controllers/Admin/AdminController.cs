d320da62271563343dbd49669de5fd5203fffaf0
duyng21303 <duyng21303@gmail.com>
2024-07-08 21:44:37 +0700

    a

--- FStep/Controllers/Admin/AdminController.cs
+++ FStep/Controllers/Admin/AdminController.cs
@@ -106,10 +106,10 @@ namespace FStep.Controllers.Admin
                             .Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Status == "Completed")
                             .CountAsync();
 				var totalPostCountExchange = await _context.Posts
-							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Type == "Exchange")
+							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Type == "Exchange" && o.Status == "Completed")
 							.CountAsync();
 				var totalPostCountSale = await _context.Posts
-							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Type == "Sale")
+							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Type == "Sale" && o.Status == "Completed")
 							.CountAsync();
 				// Thêm số lượng giao dịch hoặc giá trị 0 vào danh sách kết quả
 				resultListTotal.Add(totalTransactionCount);
