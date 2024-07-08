a5a966474ce3e5efe5c22c5eec4a9331b48c4657
Nguyễn Duy <133040402+duyng21303@users.noreply.github.com>
2024-07-09 01:45:33 +0700

    Feature/87 report comment transaction (#125)
    * list user
    
    * #51 create list user
    
    * #52 add profile orther and search by name and email
    
    * #53 create propose product
    
    * commit
    
    * commit
    
    * commit
    
    * feat: adding photos to the sale comments section, editing the sale product recommendations section
    
    * #53 update slide recomender product
    
    * #67 update upload image with comment
    
    * fg
    
    * #67 resolve conflict
    
    * a
    
    * resolve conflict
    
    * commit
    
    * feat: Preview image, preview user detail, live search
    
    * #67 live search, zoom in pic, userdetail
    
    * update report for comment and transaction
    
    * #87 update report for comment and transaction
    
    * #89 update set role for user and moder viewlist
    
    * #90 ban account follow role
    
    * #93 add point rating for user and ban when point lower than 20
    
    * commit
    
    * #87 update view report comment and delete
    
    ---------
    
    Co-authored-by: dhung2103 <hunglupu@gmail.com>

--- FStep/Controllers/Admin/AdminController.cs
+++ FStep/Controllers/Admin/AdminController.cs
@@ -2,6 +2,7 @@
 using FStep.Data;
 using FStep.Helpers;
 using FStep.Models;
+using FStep.Services;
 using FStep.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,23 +13,25 @@ using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 namespace FStep.Controllers.Admin
 {
 
-    public class AdminController : Controller
-    {
-        private readonly FstepDbContext _context;
-        private readonly IMapper _mapper;
-        private static readonly string[] defaultRole = new[] { "Customer", "Moderator", "Administrator" };
+	public class AdminController : Controller
+	{
+		private readonly FstepDbContext _context;
+		private readonly IMapper _mapper;
+		private static readonly string[] defaultRole = new[] { "Customer", "Moderator", "Admin" };
 		private readonly IConfiguration _configuration;
+		private readonly NotificationServices notificationServices;
 		public AdminController(FstepDbContext context, IMapper mapper, IConfiguration configuration)
-        {
-            _context = context;
-            _mapper = mapper;
+		{
+			_context = context;
+			_mapper = mapper;
 			_configuration = configuration;
+			notificationServices = new NotificationServices(_context);
 		}
 
-        public async Task<IActionResult> Index(string codeTransaction, int? page)
-        {
-            var totalPost = _context.Posts.Count(p => p.Status != "Rejected");
-            var totalUser = _context.Users.Count(u => u.Status != false);
+		public async Task<IActionResult> Index(string codeTransaction, int? page)
+		{
+			var totalPost = _context.Posts.Count(p => p.Status != "Rejected");
+			var totalUser = _context.Users.Count(u => u.Status != false);
 			var totalTransaction = _context.Transactions.Count(t => t.Status == "Completed" || t.Status == "Processing" || t.Status == "Canceled");
 			var totalGMV = _context.Transactions
 				.Where(t => t.Status == "Completed")
@@ -83,28 +86,28 @@ namespace FStep.Controllers.Admin
 					.ToList();
 
 
-            // Phân loại giao dịch theo tháng
-            var resultListTotalPost = new List<int>();
-            var resultListTotal = new List<int>();
-            var resultListTotalCompleted = new List<int>();
+			// Phân loại giao dịch theo tháng
+			var resultListTotalPost = new List<int>();
+			var resultListTotal = new List<int>();
+			var resultListTotalCompleted = new List<int>();
 
 			var resultListPostCountExchange = new List<int>();
 			var resultListPostCountSale = new List<int>();
 			var amount = _configuration.GetValue<float>("Amount");
 			foreach (var month in months)
-            {
-                // Kiểm tra xem tháng hiện tại có giao dịch hay không
-                var totalPostCount = await _context.Posts
-                            .Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year)
-                            .CountAsync();
+			{
+				// Kiểm tra xem tháng hiện tại có giao dịch hay không
+				var totalPostCount = await _context.Posts
+							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year)
+							.CountAsync();
 
 				var totalTransactionCount = await _context.Transactions
 							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year)
 							.CountAsync();
 
-                var totalCompletedTransactionCount = await _context.Transactions
-                            .Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Status == "Completed")
-                            .CountAsync();
+				var totalCompletedTransactionCount = await _context.Transactions
+							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Status == "Completed")
+							.CountAsync();
 				var totalPostCountExchange = await _context.Posts
 							.Where(o => o.Date != null && o.Date.Value.Month == month.Month && o.Date.Value.Year == month.Year && o.Type == "Exchange" && o.Status == "Completed")
 							.CountAsync();
@@ -113,30 +116,30 @@ namespace FStep.Controllers.Admin
 							.CountAsync();
 				// Thêm số lượng giao dịch hoặc giá trị 0 vào danh sách kết quả
 				resultListTotal.Add(totalTransactionCount);
-                resultListTotalCompleted.Add(totalCompletedTransactionCount);
-                resultListTotalPost.Add(totalPostCount);
+				resultListTotalCompleted.Add(totalCompletedTransactionCount);
+				resultListTotalPost.Add(totalPostCount);
 
-                resultListPostCountExchange.Add((int)(totalPostCountSale * amount));
+				resultListPostCountExchange.Add((int)(totalPostCountSale * amount));
 				resultListPostCountSale.Add((int)(totalPostCountSale * amount));
 			}
 			// Chuẩn bị dữ liệu cho biểu đồ
 			var labels = months.Select(g => "Tháng " + g.Month).ToList();
-            ViewBag.Labels = labels;
-            ViewBag.TotalTransactionDash = resultListTotal;
-            ViewBag.TotalPostDash = resultListTotalPost;
-            ViewBag.TotalCompleted = resultListTotalCompleted;
-            ViewBag.TotalPostExchange = resultListPostCountExchange;
+			ViewBag.Labels = labels;
+			ViewBag.TotalTransactionDash = resultListTotal;
+			ViewBag.TotalPostDash = resultListTotalPost;
+			ViewBag.TotalCompleted = resultListTotalCompleted;
+			ViewBag.TotalPostExchange = resultListPostCountExchange;
 			ViewBag.TotalPostSale = resultListPostCountSale;
 			//----------------------------------------------------------------------------------------------------Dasboard
 
 			ViewBag.TotalPost = totalPost;
-            ViewBag.TotalTransaction = totalTransaction;
-            ViewBag.TotalUser = totalUser;
-            ViewBag.TotalGMV = totalGMV;
-            ViewBag.TotalRevenues = totalRevenues;
-            ViewBag.CodeTransaction = codeTransaction;
-            return View(transactionVM);
-        }
+			ViewBag.TotalTransaction = totalTransaction;
+			ViewBag.TotalUser = totalUser;
+			ViewBag.TotalGMV = totalGMV;
+			ViewBag.TotalRevenues = totalRevenues;
+			ViewBag.CodeTransaction = codeTransaction;
+			return View(transactionVM);
+		}
 
 		public float CalculateDiscount(float? amount)
 		{
@@ -144,7 +147,7 @@ namespace FStep.Controllers.Admin
 			{
 
 				return _configuration.GetValue<float>("Amount");
-				 // 10% discount
+				// 10% discount
 			}
 			else
 			{
@@ -222,30 +225,50 @@ namespace FStep.Controllers.Admin
 
 			return View("UserDetail");
 		}
+
 		[Authorize(Roles = "Admin")]
-		public IActionResult CommentManager(int page = 1, int pageSize = 10, string? search = null)
+		public IActionResult ReportManager(int page = 1, int pageSize = 10, string? search = null)
 		{
-			var query = _context.Comments.Include(x => x.IdUserNavigation).Include(x => x.IdPostNavigation).AsQueryable();
+			var query = _context.Reports
+				.Include(x => x.IdUserNavigation) // Join với bảng người dùng
+				.Include(x => x.IdPostNavigation) // join với bảng bài đăng
+
+				.Include(x => x.IdTransactionNavigation) // join với bảng transaction
+				.ThenInclude(x => x.IdPostNavigation)
+				.ThenInclude(x => x.IdUserNavigation)
+
+				.Include(x => x.IdCommentNavigation) // join với bảng comment
+				.ThenInclude(x => x.IdUserNavigation) // Join với bảng người dùng
+				.AsQueryable();
+
 			if (!string.IsNullOrEmpty(search))
 			{
-				query = query.Where(x => x.Content.Contains(search) || x.Type.Contains(search));
+				query = query.Where(x => x.Content.Contains(search));
 			}
 
-			var comments = query.Skip((page - 1) * pageSize).Take(pageSize).Select(x => new CommentVM
+			var comments = query.Skip((page - 1) * pageSize).Take(pageSize).Select(x => new ReportVM
 			{
+				IdReport = x.IdReport,
 				IdPost = x.IdPost,
+				PostName = x.IdPostNavigation.Content,
+
 				IdUser = x.IdUser,
 				Content = x.Content,
-				Date = x.Date,
+				UserAvatar = x.IdUserNavigation.AvatarImg,
+				UserFullName = x.IdUserNavigation.Name,
+
 				IdComment = x.IdComment,
-				PostName = x.IdPostNavigation.Content,
-				Name = x.IdUserNavigation.Name,
-				Type = x.Type,
-				Img = x.Img,
-				avarImg = x.IdUserNavigation.AvatarImg
+				CommentContent = x.IdCommentNavigation.Content,
+				UserCommentFullName = x.IdCommentNavigation.IdUserNavigation.Name ?? x.IdTransactionNavigation.IdPostNavigation.IdUserNavigation.Name,
+				IdUserComment = x.IdCommentNavigation.IdUser ?? x.IdTransactionNavigation.IdPostNavigation.IdUserNavigation.IdUser,
+				UserCommentAvatar = x.IdCommentNavigation.IdUserNavigation.AvatarImg ?? x.IdTransactionNavigation.IdPostNavigation.IdUserNavigation.AvatarImg,
+
+				Date = x.Date,
+				IdTransaction = x.IdTransaction,
+				PointRating = x.IdCommentNavigation.IdUserNavigation.PointRating ?? x.IdTransactionNavigation.IdPostNavigation.IdUserNavigation.PointRating
 			}).ToList();
 
-			PagingModel<CommentVM> pagingModel = new()
+			PagingModel<ReportVM> pagingModel = new()
 			{
 				Items = comments,
 				PagingInfo = new PagingInfo
@@ -257,8 +280,9 @@ namespace FStep.Controllers.Admin
 				}
 			};
 
-			return View("CommentManager", pagingModel);
+			return View("ReportManager", pagingModel);
 		}
+
 		[HttpPost]
 		[Authorize(Roles = "Admin")]
 		public IActionResult EditAccount(string id)
@@ -300,71 +324,181 @@ namespace FStep.Controllers.Admin
 					user.Status = viewModel.Status;
 					user.BankName = viewModel.BankName;
 					user.BankAccountNumber = viewModel.BankAccountNumber;
-                    _context.SaveChanges();
-                    TempData["SuccessMessage"] = $"Tài khoản {user.Name} đã được cập nhật lại thành công";
-                    return RedirectToAction("UserManager", "Admin");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                TempData["ErrorMessage"] = "Đã xảy ra lỗi khi cập nhật lại tài khoản {user.Name}.";
-            }
-            return View(viewModel);
-        }
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public IActionResult ChangeRole([FromBody] ProfileVM user)
-        {
-            var userFound = _context.Users.FirstOrDefault(x => x.IdUser == user.IdUser);
-            if (userFound != null && defaultRole.Contains(user.Role))
-            {
-                userFound.Role = user.Role;
-                _context.Users.Update(userFound);
-                _context.SaveChanges();
-                return Ok();
-            }
-            return BadRequest();
-        }
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public IActionResult DeleteComment([FromBody] CommentVM comment)
-        {
-            var commentExisted = _context.Comments.FirstOrDefault(x => x.IdComment == comment.IdComment);
-            if (commentExisted != null)
-            {
-                _context.Comments.Remove(commentExisted);
-                _context.SaveChanges();
-                return Ok();
-            }
-            return BadRequest();
-        }
-        [Authorize(Roles = "Admin")]
-        public IActionResult Payment(int page = 1, int pageSize = 10, string? search = null)
-        {
-            var query = _context.Payments.AsQueryable();
-            if (!string.IsNullOrEmpty(search))
-            {
-                query = query.Where(x => x.IdTransaction.ToString().ToLower().Contains(search.ToLower()));
-            }
+					_context.SaveChanges();
+					TempData["SuccessMessage"] = $"Tài khoản {user.Name} đã được cập nhật lại thành công";
+					return RedirectToAction("UserManager", "Admin");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				TempData["ErrorMessage"] = "Đã xảy ra lỗi khi cập nhật lại tài khoản {user.Name}.";
+			}
+			return View(viewModel);
+		}
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public IActionResult ChangeRole([FromBody] ProfileVM user)
+		{
+			var userFound = _context.Users.FirstOrDefault(x => x.IdUser == user.IdUser);
+			if (userFound != null && defaultRole.Contains(user.Role))
+			{
+				userFound.Role = user.Role;
+				_context.Users.Update(userFound);
+				_context.SaveChanges();
+				return Ok();
+			}
+			return BadRequest();
+		}
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public IActionResult DeleteComment([FromBody] CommentVM comment)
+		{
+			var commentExisted = _context.Comments.FirstOrDefault(x => x.IdComment == comment.IdComment);
+			if (commentExisted != null)
+			{
+				_context.Comments.Remove(commentExisted);
+				_context.SaveChanges();
+				return Ok();
+			}
+			return BadRequest();
+		}
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public async Task<IActionResult> DeleteReport([FromBody] ReportVM report)
+		{
+			if (report.IdTransaction != null) return BadRequest();
+			var reportExisted = _context.Reports.FirstOrDefault(x => x.IdReport == report.IdReport);
+			if (reportExisted != null)
+			{
+				await _context.Database.BeginTransactionAsync();
+				try
+				{
+					_context.Reports.Remove(reportExisted);
 
-            var payment = query.Skip((page - 1) * pageSize).Take(pageSize).Select(x => _mapper.Map<PaymentVM>(x)).ToList();
+					var notification = await _context.Notifications.FirstOrDefaultAsync(x => x.IdComment == report.IdComment);
+					if (notification != null)
+					{
+						_context.Notifications.Remove(notification);
+					}
+
+					var comment = await _context.Comments.FirstOrDefaultAsync(x => x.IdComment == report.IdComment);
+					if (comment != null)
+					{
+						_context.Comments.Remove(comment);
+
+					}
+					await _context.SaveChangesAsync();
+					await _context.Database.CommitTransactionAsync();
+				}
+				catch (Exception ex)
+				{
+					await _context.Database.RollbackTransactionAsync();
+				}
+				return Ok();
+			}
+			return BadRequest();
+		}
+		[Authorize(Roles = "Admin")]
+		public IActionResult Payment(int page = 1, int pageSize = 10, string? search = null)
+		{
+			var query = _context.Payments.AsQueryable();
+			if (!string.IsNullOrEmpty(search))
+			{
+				query = query.Where(x => x.IdTransaction.ToString().ToLower().Contains(search.ToLower()));
+			}
+
+			var payment = query.Skip((page - 1) * pageSize).Take(pageSize).Select(x => _mapper.Map<PaymentVM>(x)).ToList();
 
 			var link = _configuration.GetValue<string>("VNPayMerchant");
 
 			PagingModel<PaymentVM> pagingModel = new()
-            {
-                Items = payment,
-                PagingInfo = new PagingInfo
-                {
-                    CurrentPage = page,
-                    ItemsPerPage = pageSize,
-                    TotalItems = query.Count(),
-                    Search = search
-                }
-            };
-            ViewBag.link = link;
-            return View("Payment",pagingModel);
-        }
-    }
+			{
+				Items = payment,
+				PagingInfo = new PagingInfo
+				{
+					CurrentPage = page,
+					ItemsPerPage = pageSize,
+					TotalItems = query.Count(),
+					Search = search
+				}
+			};
+			ViewBag.link = link;
+			return View("Payment", pagingModel);
+		}
+
+		/// <summary>
+		/// Admin xử lý trừ point người dùng bình báo cáo đánh giá
+		/// </summary>
+		/// <param name="report"></param>
+		/// <returns></returns>
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public async Task<IActionResult> PointHandler([FromBody] ReportVM report)
+		{
+			var reportExists = _context.Reports.FirstOrDefault(x => x.IdReport == report.IdReport);
+			User user = null;
+			Comment comment = null;
+			Transaction transaction = null;
+			int pointRating = 0;
+			if (reportExists != null)
+			{
+				// trừ 10 điểm của người bình luận nếu người khác báo cáo
+				if (report.IdComment != null)
+				{
+					comment = await _context.Comments.FirstOrDefaultAsync(x => x.IdComment == report.IdComment);
+					if (comment != null)
+					{
+						user = await _context.Users.FirstOrDefaultAsync(x => x.IdUser == comment.IdUser);
+						if (user != null)
+						{
+							user.PointRating -= 10;
+							pointRating += 10;
+						}
+					}
+				}
+
+				// trừ 20 điểm của người đăng bài nếu người dùng báo cáo mua hàng
+				if (report.IdTransaction != null)
+				{
+					transaction = await _context.Transactions.FirstOrDefaultAsync(x => x.IdTransaction == report.IdTransaction);
+					if (transaction != null)
+					{
+						user = await _context.Users.FirstOrDefaultAsync(x => x.IdUser == transaction.IdUserSeller);
+						if (user != null)
+						{
+							user.PointRating -= 20;
+							pointRating += 20;
+						}
+					}
+				}
+
+				if (user != null)
+				{
+					await _context.Database.BeginTransactionAsync();
+					try
+					{
+						_context.Users.Update(user);
+						await _context.SaveChangesAsync();
+						if (transaction != null)
+						{
+							await notificationServices.CreateNotification(user.IdUser, "ReportTransaction", "Transaction", pointRating.ToString(), transaction.IdTransaction);
+						}
+
+						if (comment != null)
+						{
+							await notificationServices.CreateNotification(user.IdUser, "ReportComment", "Comment", pointRating.ToString(), comment.IdComment);
+						}
+						await _context.Database.CommitTransactionAsync();
+					}
+					catch (Exception ex)
+					{
+						await _context.Database.RollbackTransactionAsync();
+					}
+				}
+				return Ok();
+			}
+			return BadRequest();
+		}
+	}
 }
