9064f4c6d9cd4bf3357ffae6100118a9212a46de
duyng21303 <duyng21303@gmail.com>
2024-07-10 01:34:55 +0700

    a

--- FStep/Repostory/Service/PostExpirationService.cs
+++ FStep/Repostory/Service/PostExpirationService.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using FStep.Repostory.Interface;
-using FStep.Data; // Sử dụng entity framework core lưu trữ data.
+using FStep.Data;
+using NuGet.Protocol.Plugins; // Sử dụng entity framework core lưu trữ data.
 
 namespace FStep.Repostory.Service
 {
@@ -73,6 +74,7 @@ namespace FStep.Repostory.Service
                                      </span> <br/>
                                      Sincerely,<br/>
                                      Admin Team";
+							dbContext.Update(post);
 							await dbContext.SaveChangesAsync();
 							await _emailSender.EmailSendAsync(post.IdUserNavigation.Email, subject, body);
 							_logger.LogInformation($"The email has been sent successfully to {post.IdUserNavigation.Email}");
@@ -83,6 +85,133 @@ namespace FStep.Repostory.Service
 							throw;
 						}
 					}
+				}
+				// Các transaction exchange bị quá hạn 3 ngày (1 trong 2 không đem hàng tới, huỷ tự động)
+				var exchange = await dbContext.Transactions.Where(p => p.Type == "Exchange" && p.Status == "Processing" && p.Date.Value.AddDays(3).CompareTo(DateTime.Now) < 0 && (p.SentImg == null || p.SentBuyerImg == null))?.ToListAsync();
+				foreach (var x in exchange)
+				{
+					//update status transaction, post, product, payment
+					x.Status = "Canceled";
+					dbContext.Update(x);
+
+					var seller = await dbContext.Users.SingleOrDefaultAsync(p => p.IdUser == x.IdUserSeller);
+					var buyer = await dbContext.Users.SingleOrDefaultAsync(p => p.IdUser == x.IdUserBuyer);
+
+					var post = dbContext.Posts.SingleOrDefault(p => p.IdPost == x.IdPost);
+					post.Status = "True";
+					dbContext.Update(post);
+
+					var product = dbContext.Products.SingleOrDefault(p => p.IdProduct == post.IdProduct);
+					product.Status = "True";
+					dbContext.Update(product);
+
+					var payment = dbContext.Payments.FirstOrDefault(p => p.IdTransaction == x.IdTransaction);
+					payment.Status = "False";
+					payment.CancelDate = DateTime.Now;
+					dbContext.Update(payment);
+
+					//Update point rating user
+					if (x.SentImg == null)
+					{
+						if (x.SentBuyerImg == null)
+						{
+							seller.PointRating -= 15;
+							buyer.PointRating -= 15;
+						}
+						else
+						{
+							seller.PointRating -= 15;
+						}
+					}
+					else
+					{
+						buyer.PointRating -= 15;
+					}
+					//send email inform for user
+					string body = $"Đơn hàng mới mã số <span style=\"color:red\"> {x.CodeTransaction} </span> đã bị huỷ tự động vì quá hạn giao hàng, vui lòng tới nhận lại hàng của bạn!!!";
+					await _emailSender.EmailSendAsync(seller.Email, "Đơn hàng bị huỷ tự động vì quá hạn", body);
+					await _emailSender.EmailSendAsync(buyer.Email, "Đơn hàng bị huỷ tự động vì quá hạn", body);
+
+					//save change db
+					await dbContext.SaveChangesAsync();
+				}
+				// Các transaction sale bị quá hạn 3 ngày (người bán không đem hàng tới, huỷ tự động)
+				var sale = await dbContext.Transactions.Where(p => p.Type == "Sale" && p.Status == "Processing" && p.Date.Value.AddDays(3).CompareTo(DateTime.Now) < 0 && p.SentImg == null)?.ToListAsync();
+				foreach (var x in sale)
+				{
+					//update status transaction, post, product, payment
+					x.Status = "Canceled";
+					dbContext.Update(x);
+
+					var seller = await dbContext.Users.SingleOrDefaultAsync(p => p.IdUser == x.IdUserSeller);
+					var buyer = await dbContext.Users.SingleOrDefaultAsync(p => p.IdUser == x.IdUserBuyer);
+
+					var post = dbContext.Posts.SingleOrDefault(p => p.IdPost == x.IdPost);
+					post.Status = "True";
+					dbContext.Update(post);
+
+					var product = dbContext.Products.SingleOrDefault(p => p.IdProduct == post.IdProduct);
+					product.Status = "True";
+					dbContext.Update(product);
+
+					var payment = dbContext.Payments.FirstOrDefault(p => p.IdTransaction == x.IdTransaction);
+					payment.Status = "False";
+					payment.CancelDate = DateTime.Now;
+					dbContext.Update(payment);
+
+					//Update point rating user
+					seller.PointRating -= 15;
+
+					// refund cho người mua
+					//
+					// renfund end
+
+					//send email inform for user
+					await _emailSender.EmailSendAsync(seller.Email, "Đơn hàng bị huỷ tự động vì quá hạn", $"Đơn hàng mới mã số <span style=\"color:red\"> {x.CodeTransaction} </span> đã bị huỷ tự động vì quá hạn giao hàng. <span style=\"color:red\">Bạn sẽ bị trừ điểm uy tín</span>, xin hãy lưu ý!!!");
+					await _emailSender.EmailSendAsync(buyer.Email, "Đơn hàng bị huỷ tự động vì quá hạn", $"Đơn hàng mới mã số <span style=\"color:red\"> {x.CodeTransaction} </span> đã bị huỷ tự động vì quá hạn giao hàng, tiền của bạn sẽ được hoàn lại trong thời gian sớm nhất!!!");
+
+					//save change db
+					await dbContext.SaveChangesAsync();
+				}
+				// Tất cả Các transaction processing bị quá hạn 7 ngày (hoàn thành giao dịch)
+				var transaction = await dbContext.Transactions.Where(p => p.Status == "Processing" && p.Date.Value.AddDays(7).CompareTo(DateTime.Now) < 0)?.ToListAsync();
+				foreach (var x in transaction)
+				{
+					//update status transaction, post, product, payment
+					x.Status = "Completed";
+					dbContext.Update(x);
+
+					var seller = await dbContext.Users.SingleOrDefaultAsync(p => p.IdUser == x.IdUserSeller);
+					var buyer = await dbContext.Users.SingleOrDefaultAsync(p => p.IdUser == x.IdUserBuyer);
+
+					var post = dbContext.Posts.SingleOrDefault(p => p.IdPost == x.IdPost);
+					post.Status = "False";
+					dbContext.Update(post);
+
+					var product = dbContext.Products.SingleOrDefault(p => p.IdProduct == post.IdProduct);
+					product.Status = "False";
+					dbContext.Update(product);
+
+					var payment = new Payment();
+					payment.Status = "True";
+					payment.PayTime = DateTime.Now;
+					payment.IdTransaction = x.IdTransaction;
+					payment.Amount = x.Amount ?? null;
+					payment.Type = "Seller";
+					dbContext.Add(payment);
+
+					//Chuyển tiền cho người bán
+					//
+					//Pay end
+
+					//send email inform for user
+					string body = $"Đơn hàng mới mã số <span style=\"color:green\"> {x.CodeTransaction} </span> đã được giao thành công, vui lòng kiểm tra đơn hàng trên lịch sử giao dịch của bạn!!!";
+					await _emailSender.EmailSendAsync(seller.Email, "Đơn hàng của bạn đã được giao thành công", body);
+					await _emailSender.EmailSendAsync(buyer.Email, "Đơn hàng của bạn đã được giao thành công", body);
+					dbContext.Update(x);
+
+					//save change db
+					await dbContext.SaveChangesAsync();
 				}
 			}
 		}
