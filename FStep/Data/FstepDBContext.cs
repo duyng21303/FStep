b968a43fa2dd566eca6f8b1162fa4700ab30a16f
duyng21303 <duyng21303@gmail.com>
2024-07-21 18:52:04 +0700

    resolve

--- FStep/Data/FstepDBContext.cs
+++ FStep/Data/FstepDBContext.cs
@@ -39,9 +39,6 @@ public partial class FstepDBContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=MSITRONGLE\\TRONGLE;Initial Catalog=FSTEP;Persist Security Info=True;User ID=sa;Password=12345;Encrypt=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
